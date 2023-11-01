f354910db75888fc55c068f35b3b1960c4eb3e0e
Данил <danikkach666@gmail.com>
2023-11-01 19:57:14 +0200

    add

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  let sum = quantity * pricePerDroid;
+  if (sum > customerCredits) {
+    console.log('Insufficient funds!');
+  } else {
+    console.log(`You ordered ${quantity} droids worth ${sum} credits!`);
+  }
+  return sum;
+}
+
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
